51a7cd45e53f075556a89bc06df4002221303613
gregoryyu <yugreg@umich.edu>
2025-07-25 21:08:33 -0400

    support screen clearing

--- src/chip8.cc
+++ src/chip8.cc
@@ -43,6 +43,7 @@ bool Chip8::loadRom(const std::filesystem::path &path) {
 }
 
 bool Chip8::execute_cycle() {
+  redraw_ = false;
   std::byte b1 = memory_[program_counter_];
   std::byte b2 = memory_[program_counter_ + 1];
   std::cout << "The instruction for this cycle is: " << std::hex << std::setw(2)
@@ -50,8 +51,13 @@ bool Chip8::execute_cycle() {
             << std::hex << std::setw(2) << std::setfill('0')
             << static_cast<unsigned int>(b2)
             << " at address: " << program_counter_ << std::endl;
+  uint8_t opcode = static_cast<uint8_t>(b1 >> 4);
+  // CLS - Clear Screen
+  if (opcode == 0x00 && static_cast<uint8_t>(b2) == 0xE0) {
+    display_ = std::array<uint8_t, 64 * 32>{};
+    redraw_ = true;
+  }
   program_counter_ += 2;
-  redraw_ = true;
   return true;
 }
 
