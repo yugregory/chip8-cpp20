ae8ea0e231e522ed878d255fd98d0f76f29ca7a5
gregoryyu <yugreg@umich.edu>
2025-07-26 17:46:11 -0400

    add support for concepts and separate visual system

---
+++ src/SDL_system.h
@@ -0,0 +1,34 @@
+#ifndef SRC_SDL_SYSTEM_H
+#define SRC_SDL_SYSTEM_H
+
+#include "V_system.h"
+#include "chip8.h"
+#include <SDL3/SDL.h>
+#include <array>
+#include <memory>
+
+namespace chip8 {
+
+class SDLSystem {
+public:
+  SDLSystem(int width, int height, int scale);
+  ~SDLSystem();
+  void poll_events(bool &quit);
+  void draw(const Chip8 &chip8);
+
+private:
+  int width_;
+  int height_;
+  int scale_;
+  std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window_;
+  std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> renderer_;
+  std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)> texture_;
+  std::array<uint8_t, 64 * 32> screen_;
+};
+
+// Statically assert that SDLSystem satisfies the AVSystem concept.
+static_assert(VSystem<SDLSystem>);
+
+} // namespace chip8
+
+#endif
