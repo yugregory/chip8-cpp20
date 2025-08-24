8a6f3729f37d2d7bdedccdefb5c1e06c94370e3c
gregoryyu <yugreg@umich.edu>
2025-08-24 15:54:42 -0400

    add arm neon support to chip8 and ran against basic tests

--- src/chip8.cc
+++ src/chip8.cc
@@ -91,11 +91,11 @@ common::Status scalar_draw(chip8::Chip8 &em, uint8_t b1, uint8_t b2) {
   int y_coord = em.registers_[Vy] % 32;
   em.registers_[0xFu] = 0u;
   for (uint8_t i = 0u; i < height; i++) {
-    uint8_t sprite_byte =
-        static_cast<uint8_t>(em.memory_[em.index_register_ + i]);
     int y = (y_coord + i);
     if (y >= 32)
       break;
+    uint8_t sprite_byte =
+        static_cast<uint8_t>(em.memory_[em.index_register_ + i]);
     for (uint8_t j = 0u; j < 8u; j++) {
       if ((sprite_byte & (0x80u >> j))) {
         int x = (x_coord + j);
@@ -118,7 +118,7 @@ common::Status vector_draw(chip8::Chip8 &em, uint8_t b1, uint8_t b2) {
   uint8_t height = (b2 & 0xFu);
   int x_coord = em.registers_[Vx] % 64;
   // Scalar draw to prevent any overflow for simd
-  if (x_coord + 8 > 64) {
+  if (x_coord + 8 >= 64) {
     return scalar_draw(em, b1, b2);
   }
   int y_coord = em.registers_[Vy] % 32;
@@ -127,12 +127,13 @@ common::Status vector_draw(chip8::Chip8 &em, uint8_t b1, uint8_t b2) {
     int y = (y_coord + i);
     if (y >= 32)
       break;
-    const std::array<uint8_t, 8> &sprite = em.sprite_table_[y];
-    int start_pixel = y * 32 + x_coord;
+    uint8_t sprite_byte =
+        static_cast<uint8_t>(em.memory_[em.index_register_ + i]);
+    int start_pixel = y * 64 + x_coord;
     uint8x8_t display_pixels = vld1_u8(&em.display_[start_pixel]);
-    uint8x8_t sprite_mask = vld1_u8(sprite.data());
+    uint8x8_t sprite_mask = vld1_u8(em.sprite_table_[sprite_byte].data());
     uint8_t max_val_in_display = vmaxv_u8(display_pixels);
-    if (max_val_in_display > 0) {
+    if (max_val_in_display == 1u) {
       em.registers_[0xFu] = 1u;
     }
     uint8x8_t result = veor_u8(display_pixels, sprite_mask);
@@ -375,7 +376,7 @@ Chip8::Chip8()
   for (size_t i = 0; i < k_font_space; i++) {
     memory_[k_font_address + i] = static_cast<std::byte>(k_fontset[i]);
   }
-  for (size_t i = 0; i < 32; i++) {
+  for (size_t i = 0; i < 256; i++) {
     for (size_t j = 0; j < 8; j++) {
       sprite_table_[i][j] = (i & (0x80u >> j)) ? 1u : 0u;
     }
